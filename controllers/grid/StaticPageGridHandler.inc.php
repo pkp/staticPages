374d070dfe3fff9b5957719a7adfcbb1f4e02f1b
Alec Smecher <alec@smecher.bc.ca>
2018-08-29 14:54:21 -0700

    pkp/pkp-lib#4024 Add ContextAccessPolicy

--- controllers/grid/StaticPageGridHandler.inc.php
+++ controllers/grid/StaticPageGridHandler.inc.php
@@ -45,7 +45,16 @@ class StaticPageGridHandler extends GridHandler {
 	// Overridden template methods
 	//
 	/**
-	 * @copydoc Gridhandler::initialize()
+	 * @copydoc PKPHandler::authorize()
+	 */
+	function authorize($request, &$args, $roleAssignments) {
+		import('lib.pkp.classes.security.authorization.ContextAccessPolicy');
+		$this->addPolicy(new ContextAccessPolicy($request, $roleAssignments));
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+
+	/**
+	 * @copydoc GridHandler::initialize()
 	 */
 	function initialize($request, $args = null) {
 		parent::initialize($request);
