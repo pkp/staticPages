dc5c4b58c349947701b8f721e61c14a8c58e2935
Touhidur Rahman <abir.ruet@gmail.com>
2023-01-25 09:46:47 +0600

    pkp/pkp-lib#8523 Test file copyright date update

--- cypress/tests/functional/StaticPages.cy.js
+++ cypress/tests/functional/StaticPages.cy.js
@@ -1,8 +1,8 @@
 /**
  * @file cypress/tests/functional/StaticPages.cy.js
  *
- * Copyright (c) 2014-2020 Simon Fraser University
- * Copyright (c) 2000-2020 John Willinsky
+ * Copyright (c) 2014-2023 Simon Fraser University
+ * Copyright (c) 2000-2023 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  */
