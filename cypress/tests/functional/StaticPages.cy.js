e896f32b8eae3f8de35197467cf7e5aab25bbb5e
Alec Smecher <alec@smecher.bc.ca>
2023-01-27 12:54:21 -0800

    Add wait for form to settle

--- cypress/tests/functional/StaticPages.cy.js
+++ cypress/tests/functional/StaticPages.cy.js
@@ -31,6 +31,7 @@ describe('Static Pages plugin tests', function() {
 
 		// Create a static page
 		cy.get('a[id^="component-plugins-generic-staticpages-controllers-grid-staticpagegrid-addStaticPage-button-"]').click();
+		cy.waitJQuery(2000); // Wait for form to settle
 		cy.get('form[id="staticPageForm"] input[id^="path-"]').type('flarm', { delay: 0});
 		cy.get('form[id^="staticPageForm"] input[id^="title-en_US-"]').type('Test Static Page', { delay: 0});
 		cy.get('textarea[id^="content-en_US-"]').then(node => {
